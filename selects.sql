fea4cdf6480278d4174f6a20a3dc82fa212cac90
Rapidash99 <slavav99@mail.ru>
2018-11-23 20:41:07 +0300

    changes in car entity; selects.sql

--- selects.sql
+++ selects.sql
@@ -8,4 +8,8 @@ SELECT using_start, using_end FROM ride
 --5
 SELECT using_start, using_end, coordinate_a, coordinate_b FROM ride
 
+--6
+SELECT using_start, coordinate_a, coordinate_b FROM ride
+WHERE using_start [TIME] [HOUR] (>= 7 AND <= 10) OR (>=12 AND <=14) OR (>=17 AND <=19)
+
 --
